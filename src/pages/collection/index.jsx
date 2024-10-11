e1d601907b074ddde04efa852d52031c95adbd5c
freewindias <freewindias6@gmail.com>
2024-10-11 20:56:13 +0530

    f

--- src/pages/collection/index.jsx
+++ src/pages/collection/index.jsx
@@ -4,66 +4,74 @@ import { gsap } from "gsap";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
+
 export default function Collection() {
-  const [categories, setCategories] = useState([]); // State to hold categories
+ const [categories, setCategories] = useState([]); // State to hold categories
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await fetch("/api/categories"); // Fetch categories from the API
-        if (!response.ok) throw new Error("Failed to fetch categories");
-        const data = await response.json();
 
-        console.log("Fetched categories:", data); // Log fetched categories for debugging
+ useEffect(() => {
+   const fetchCategories = async () => {
+     try {
+       const response = await fetch("/api/categories"); // Fetch categories from the API
+       if (!response.ok) throw new Error("Failed to fetch categories");
+       const data = await response.json();
 
-        setCategories(data); // Set the fetched categories
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
 
-    fetchCategories();
-  }, []);
+       console.log("Fetched categories:", data); // Log fetched categories for debugging
 
-  const manageMouseEnter = (e, index) => {
-    gsap.to(e.target, {
-      top: "-2vw",
-      backgroundColor: "lightgray", // Adjust color as needed
-      duration: 0.3,
-    });
-  };
 
-  const manageMouseLeave = (e) => {
-    gsap.to(e.target, {
-      top: "0",
-      backgroundColor: "white",
-      duration: 0.3,
-      delay: 0.1,
-    });
-  };
+       setCategories(data); // Set the fetched categories
+     } catch (error) {
+       console.error("Error fetching categories:", error);
+     }
+   };
 
-  return (
-    <Layout title="Art Collections">
-      <div className={styles.container}>
-        <div className={styles.collectionContainer}>
-          <p className={styles.heading}>Collections:</p>
-          {categories.map((category) => (
-            <Link
-              href={`/categories/${category.name}`} // Use category name for routing
-              key={category._id} // Use unique ID as key
-              className={styles.linkz}
-            >
-              <div
-                className={styles.collection}
-                onMouseEnter={(e) => manageMouseEnter(e, category._id)}
-                onMouseLeave={manageMouseLeave}
-              >
-                <p>{category.name}</p> {/* Displaying category name */}
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </Layout>
-  );
+
+   fetchCategories();
+ }, []);
+
+
+ const manageMouseEnter = (e, index) => {
+   gsap.to(e.target, {
+     top: "-2vw",
+     backgroundColor: "transparent", // Adjust color as needed
+     duration: 0.3,
+   });
+ };
+
+
+ const manageMouseLeave = (e) => {
+   gsap.to(e.target, {
+     top: "0",
+     backgroundColor: "transparent",
+     duration: 0.3,
+     delay: 0.1,
+   });
+ };
+
+
+ return (
+   <Layout>
+     <div className={styles.container}>
+       <div className={styles.collectionContainer}>
+         <p className={styles.heading}>Collections:</p>
+         {categories.map((category) => (
+           <Link
+             href={`/categories/${category.name}`} // Use category name for routing
+             key={category._id} // Use unique ID as key
+             className={styles.linkz}
+           >
+             <div
+               className={styles.collection}
+               onMouseEnter={(e) => manageMouseEnter(e, category._id)}
+               onMouseLeave={manageMouseLeave}
+             >
+               <p>{category.name}</p> {/* Displaying category name */}
+             </div>
+           </Link>
+         ))}
+       </div>
+     </div>
+   </Layout>
+ );
 }
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,112 +1,120 @@
 // src/pages/contact.js
 
+
 import { useState } from "react";
 import styles from "./Contact.module.scss"; // Create this file for your styles
 import Layout from "@/components/layout";
 import ContactPage from "@/components/contactpage";
 const accessKey = process.env.WEB3FORMS_ACCESS_KEY;
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+ const [formData, setFormData] = useState({
+   name: "",
+   email: "",
+   message: "",
+ });
+ const [successMessage, setSuccessMessage] = useState("");
+ const [errorMessage, setErrorMessage] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-  const accessKey = process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY;
-  const handleSubmit = async (event) => {
-    event.preventDefault();
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        ...formData,
-        access_key: accessKey, // Access key as a string
-      }),
-    });
+ const handleChange = (e) => {
+   const { name, value } = e.target;
+   setFormData({ ...formData, [name]: value });
+ };
+ const accessKey = process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY;
+ const handleSubmit = async (event) => {
+   event.preventDefault();
 
-    const result = await response.json();
 
-    if (result.success) {
-      setSuccessMessage(
-        "Thank you for contacting us! We'll get back to you soon."
-      );
-      setErrorMessage("");
-      setFormData({ name: "", email: "", message: "" }); // Reset form
-    } else {
-      setErrorMessage(
-        "There was an error sending your message. Please try again."
-      );
-      setSuccessMessage("");
-    }
-  };
+   const response = await fetch("https://api.web3forms.com/submit", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       Accept: "application/json",
+     },
+     body: JSON.stringify({
+       ...formData,
+       access_key: accessKey, // Access key as a string
+     }),
+   });
 
-  return (
-    <Layout title="Contact Us">
-      <div className={styles.contactContainer}>
-        <h1 className={styles.head}>Contact Us</h1>
-        <form onSubmit={handleSubmit} className={styles.contactForm}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-          <button type="submit">Send Message</button>
-        </form>
-        {successMessage && <p className={styles.success}>{successMessage}</p>}
-        {errorMessage && <p className={styles.error}>{errorMessage}</p>}
 
-        {/* SVG Decoration */}
-        {/* <svg
-        className={styles.svgDecoration}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-        >
-        <circle cx="50" cy="50" r="40" fill="#ff6347" opacity="0.5" />
-        <path
-        d="M10,10 C40,40 60,40 90,10"
-        stroke="#ff6347"
-        strokeWidth="2"
-        fill="none"
-        />
-        <path
-        d="M10,90 C40,60 60,60 90,90"
-        stroke="#ff6347"
-        strokeWidth="2"
-        fill="none"
-        />
-        </svg> */}
-      </div>
-      <ContactPage />
-    </Layout>
-  );
+   const result = await response.json();
+
+
+   if (result.success) {
+     setSuccessMessage(
+       "Thank you for contacting us! We'll get back to you soon."
+     );
+     setErrorMessage("");
+     setFormData({ name: "", email: "", message: "" }); // Reset form
+   } else {
+     setErrorMessage(
+       "There was an error sending your message. Please try again."
+     );
+     setSuccessMessage("");
+   }
+ };
+
+
+ return (
+   <Layout>
+     <div className={styles.contactContainer}>
+       <h1 className={styles.head}>Contact Us</h1>
+       {/* <form onSubmit={handleSubmit} className={styles.contactForm}>
+         <input
+           type="text"
+           name="name"
+           placeholder="Your Name"
+           value={formData.name}
+           onChange={handleChange}
+           required
+         />
+         <input
+           type="email"
+           name="email"
+           placeholder="Your Email"
+           value={formData.email}
+           onChange={handleChange}
+           required
+         />
+         <textarea
+           name="message"
+           placeholder="Your Message"
+           value={formData.message}
+           onChange={handleChange}
+           required
+         ></textarea>
+         <button type="submit">Send Message</button>
+       </form>
+       {successMessage && <p className={styles.success}>{successMessage}</p>}
+       {errorMessage && <p className={styles.error}>{errorMessage}</p>} */}
+
+
+       {/* SVG Decoration */}
+       {/* <svg
+       className={styles.svgDecoration}
+       xmlns="http://www.w3.org/2000/svg"
+       viewBox="0 0 100 100"
+       >
+       <circle cx="50" cy="50" r="40" fill="#ff6347" opacity="0.5" />
+       <path
+       d="M10,10 C40,40 60,40 90,10"
+       stroke="#ff6347"
+       strokeWidth="2"
+       fill="none"
+       />
+       <path
+       d="M10,90 C40,60 60,60 90,90"
+       stroke="#ff6347"
+       strokeWidth="2"
+       fill="none"
+       />
+       </svg> */}
+     </div>
+     <ContactPage/>
+   </Layout>
+ );
 };
+
 
 export default Contact;
