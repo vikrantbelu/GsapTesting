166a79e0de2a206fe24d9e2cab7d733e7dc49176
AlesterR <alesterrods@gmail.com>
2024-10-21 23:09:37 +0400

    buttonedit

--- src/pages/categories/[slug].js
+++ src/pages/categories/[slug].js
@@ -18,9 +18,9 @@ const CategoryPage = ({ products, slug }, suppressHydrationWarning) => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                fill="white"
-                width="24px"
-                height="24px"
+                fill="black"
+                width="12px"
+                height="12px"
               >
                 <path d="M0 0h24v24H0V0z" fill="none" />
                 <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6l6 6l1.41-1.41z" />
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -23,9 +23,9 @@ const ProductPage = ({ product }) => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill="white"
-              width="24px"
-              height="24px"
+              fill="black"
+              width="12px"
+              height="12px"
             >
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6l6 6l1.41-1.41z" />
